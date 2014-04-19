70ba1f875cb514d46215ba3ca23480b76c000546
root <root@ponginfinity.com>
2014-04-19 14:52:11 -0400

    made leave work

--- server/public/js/app.controllers.js
+++ server/public/js/app.controllers.js
@@ -12,7 +12,6 @@ app.controller('GameCtrl', function($scope, $routeParams, SocketSvc) {
 			// When we just got here
 			$scope.state = payload.state;
 			$scope.me = $scope.state[$scope.state.length - 1];
-			$scope.me.size = $scope.me.size || 360;
 			console.log('state', payload);
 		});
 		SocketSvc.on('newPlayer', function(payload) {
@@ -21,16 +20,25 @@ app.controller('GameCtrl', function($scope, $routeParams, SocketSvc) {
 			var runningTotal = 0;
 			for (var i = 0; i < $scope.state.length; i++) {
 				$scope.state[i].size *= multiplier;
+				$scope.state[i].paddlePosition *= multiplier;
 				runningTotal += $scope.state[i].size;
 			}
 			var player = {
 				size: (360.0 - runningTotal),
+				paddlePosition: ((360.0 - runningTotal) / 2.0),
 				id: payload.playerId
 			};
 			$scope.state.push(player);
 		});
 		SocketSvc.on('playerLeave', function(payload){
-			$scope.splice(payload.playerIndex, 1);
+			var vacantSpace = $scope.state[payload.playerIndex].size;
+			$scope.state.splice(payload.playerIndex, 1);
+			var multiplier = (360.0 / (360.0 - vacantSpace)); 
+			for (var i = 0; i < $scope.state.length; i++) {
+				$scope.state[i].size *= multiplier;
+				$scope.state[i].paddlePosition *= multiplier;
+			}
+			console.log('leave', $scope.state);
 		});
 	};
 });
